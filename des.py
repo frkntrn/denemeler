d6b7160958567487cb31ffeff3a8565cac44ef4e
frkntrn <frkntrn.hck@gmail.com>
2018-02-19 03:36:09 +0300

    Update des.py

--- des.py
+++ des.py
@@ -24,6 +24,9 @@ def cilingir(anahtar):
     sol = sol[i:] + sol[:i]
     sag = sag[i:] + sag[:i]
     anahtarlik += [sol+sag]
-  return anahtarlik
+  #2. anahtar karma listesi
+  ak2 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
+  #anahtarlari bu listeden gecirip son hallerini olustur
+  return map(lambda x: kar(x,ak2),anahtarlik)
       
 
