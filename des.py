0c5cf4eb63b933113d9f8e8d90aef6eb0cae7080
frkntrn <frkntrn.hck@gmail.com>
2018-02-19 17:53:19 +0300

    Update des.py

--- des.py
+++ des.py
@@ -1,7 +1,5 @@
 #kaynak: http://page.math.tu-berlin.de/~kant/teaching/hess/krypto-ws2006/des.htm
 
-def des(metin, anahtar):
-
 #altiliyi ikiliye donustur
 altiki= lambda x: "".join(["{:04b}".format(int(i,16)) for i in x])
 
@@ -53,7 +51,17 @@ def cilingir(anahtar):
   return map(lambda x: kar(x,ak2),anahtarlik)
       
 def sifrele(metin, anahtar):
-  #metin karma listesi
-  mk = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
-  m = kar(metin, mk)
+  #1. metin karma listesi
+  mk1 = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
+  m = kar(metin, mk1)
+  #anahtarligi olustur
+  anahtarlik = cilingir(anahtar)
+  #baslangic sag ve sol degerlerini olustur
+  sol = m[:len(m)/2]; sag = m[len(m)/2:]
+  #16 aşamadan geçirip şifrele
+  for anahtar in anahtarlik: sol, sag = sag, xor(sol, f(sag, anahtar))
+  #2. metin karma listesi
+  mk2 = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25]
+  #bu listeyle kar
+  return kar(sag+sol, mk2)
   
